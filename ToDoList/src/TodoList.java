c3ac1a2f2990296535f6973726b997ad866313d2
Hidan404 <91800966+Hidan404@users.noreply.github.com>
2024-03-19 18:21:37 -0300

    up2

---
+++ ToDoList/src/TodoList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+    private List<String> items = new ArrayList<>();
+
+    public TodoList(List<String> items) {
+        this.items = items;
+    }
+
+    public void adicionarItem(String e){
+        items.add(e);
+    }
+
+    public void removerItem(int i){
+        if(i > 0 && i < items.size()){
+            String removido = items.remove(i);
+        }
+    }
+}
