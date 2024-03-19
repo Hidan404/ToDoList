7d3ed6d580d25d07a86ccb36ba39bce7e8ef373c
Hidan404 <91800966+Hidan404@users.noreply.github.com>
2024-03-19 18:25:22 -0300

    up3

--- ToDoList/src/TodoList.java
+++ ToDoList/src/TodoList.java
@@ -15,6 +15,13 @@ public class TodoList {
     public void removerItem(int i){
         if(i > 0 && i < items.size()){
             String removido = items.remove(i);
+            System.out.println("Item removido " + removido);
+        }
+    }
+
+    public void listarItemns(){
+        for (String string : items) {
+            System.out.println(string);
         }
     }
 }
